f82a7ca75e7e2923d43a053e860bd8d69e3036ad
CrackTC <chenrke369@gmail.com>
2020-12-03 20:50:53 +0800

    Add configuration file for fish shell

---
+++ fish/config.fish
@@ -0,0 +1,8 @@
+export EDITOR='nvim'
+export fish_greeting=''
+export FZF_DEFAULT_COMMAND='fd --type file'
+export FZF_CTRL_T_COMMAND='$FZF_DEFAULT_COMMAND'
+export FZF_DEFAULT_COMMAND='fd --type file --color=always --follow --hidden --exclude .git'
+export FZF_DEFAULT_OPTS='--ansi'
+export BAT_THEME='Nord'
+# vim:ft=sh
---
+++ fish/functions/d.fish
@@ -0,0 +1,4 @@
+# Defined in - @ line 1
+function d --wraps=disown --description 'alias d disown'
+  disown  $argv;
+end
