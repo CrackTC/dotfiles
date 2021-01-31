796f3e02aa164b8e665308ce745601db297668e0
CrackTC <chenrke369@gmail.com>
2021-01-31 09:14:08 +0800

    fix dotnet issue

--- fish/config.fish
+++ fish/config.fish
@@ -5,4 +5,5 @@ export FZF_CTRL_T_COMMAND='$FZF_DEFAULT_COMMAND'
 export FZF_DEFAULT_COMMAND='fd --type file --color=always --follow --hidden --exclude .git'
 export FZF_DEFAULT_OPTS='--ansi'
 export BAT_THEME='Nord'
+export FrameworkPathOverride=/lib/mono/4.5
 # vim:ft=sh
