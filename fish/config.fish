e18baba58acbfbe631efa0e17601301cf6e622c6
CrackTC <chenrke369@gmail.com>
2023-01-05 21:21:30 +0800

    bat: add true color support

--- fish/config.fish
+++ fish/config.fish
@@ -5,5 +5,6 @@ export FZF_DEFAULT_COMMAND='fd --type file --follow --hidden --exclude .git'
 # -1 indicates using default color of terminal
 export FZF_DEFAULT_OPTS='--ansi --color=fg:#e5e9f0,bg:-1,hl:#81a1c1 --color=fg+:#e5e9f0,bg+:-1,hl+:#81a1c1 --color=info:#eacb8a,prompt:#bf6069,pointer:#b48dac --color=marker:#a3be8b,spinner:#b48dac,header:#a3be8b'
 export BAT_THEME='Nord'
+export COLORTERM='24bit'
 export FrameworkPathOverride=/lib/mono/4.5
 # vim:ft=sh
