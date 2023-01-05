9cd038572c9dd060ecf5e51b4619f5e0001e017f
CrackTC <chenrke369@gmail.com>
2023-01-05 20:09:58 +0800

    config theme of fzf

--- fish/config.fish
+++ fish/config.fish
@@ -1,9 +1,9 @@
 export EDITOR='nvim'
 export fish_greeting=''
-export FZF_DEFAULT_COMMAND='fd --type file'
 export FZF_CTRL_T_COMMAND='$FZF_DEFAULT_COMMAND'
-export FZF_DEFAULT_COMMAND='fd --type file --color=always --follow --hidden --exclude .git'
-export FZF_DEFAULT_OPTS='--ansi'
+export FZF_DEFAULT_COMMAND='fd --type file --follow --hidden --exclude .git'
+# -1 indicates using default color of terminal
+export FZF_DEFAULT_OPTS='--ansi --color=fg:#e5e9f0,bg:-1,hl:#81a1c1 --color=fg+:#e5e9f0,bg+:-1,hl+:#81a1c1 --color=info:#eacb8a,prompt:#bf6069,pointer:#b48dac --color=marker:#a3be8b,spinner:#b48dac,header:#a3be8b'
 export BAT_THEME='Nord'
 export FrameworkPathOverride=/lib/mono/4.5
 # vim:ft=sh
