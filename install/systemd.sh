f503ad0c290fc9ca212112d23cf48c6ce9ccadf2
CrackTC <chenrke369@gmail.com>
2023-01-30 21:23:15 +0800

    systemd: renewed install script and moved it

---
+++ install/systemd.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/sh
+sudo pacman -S --noconfirm samba
+
+path="$(cd "$(dirname $0)";cd ..;pwd)"
+
+sudo ln -snf $path/systemd/home-chen-smb.mount /etc/systemd/system/home-chen-smb.mount
+sudo ln -snf $path/systemd/home-chen-smb.automount /etc/systemd/system/home-chen-smb.automount
+
+mkdir -p /etc/samba/credentials
+sudo ln -snf $path/systemd/vps-smb /etc/samba/credentials/vps-smb
+
+sudo systemctl daemon-reload
+sudo systemctl enable home-chen-smb.automount
+sudo systemctl start home-chen-smb.automount
--- systemd/install.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/sh
-sudo pacman -S samba
-sudo mv ./home-chen-smb.mount /etc/systemd/system/
-mkdir -p /etc/samba/credentials
-sudo mv ./vps-smb /etc/samba/credentials/
