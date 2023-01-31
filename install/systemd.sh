84ab0729b0fa5c9cf76fd768f02bf08806a184f7
CrackTC <chenrke369@gmail.com>
2023-01-31 10:13:41 +0800

    install: use cp to avoid troubles

--- install/systemd.sh
+++ install/systemd.sh
@@ -3,8 +3,8 @@ sudo pacman -S --noconfirm samba
 
 path="$(cd "$(dirname $0)";cd ..;pwd)"
 
-sudo ln -snf $path/systemd/home-chen-smb.mount /etc/systemd/system/home-chen-smb.mount
-sudo ln -snf $path/systemd/home-chen-smb.automount /etc/systemd/system/home-chen-smb.automount
+sudo cp --remove-destination $path/systemd/home-chen-smb.mount /etc/systemd/system/home-chen-smb.mount
+sudo cp --remove-destination $path/systemd/home-chen-smb.automount /etc/systemd/system/home-chen-smb.automount
 
 mkdir -p /etc/samba/credentials
 sudo ln -snf $path/systemd/vps-smb /etc/samba/credentials/vps-smb
