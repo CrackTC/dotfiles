4612163c233549e92401db7aa6f907d4233c18f1
CrackTC <chenrke369@gmail.com>
2023-04-21 01:42:04 +0800

    polybar: use script for better network activity display

--- polybar/network-traffic.sh
+++ polybar/network-traffic.sh
@@ -12,21 +12,21 @@ print_bytes() {
     echo "$bytes"
 }
 
-print_bit() {
-    if [ "$1" -eq 0 ] || [ "$1" -lt 10 ]; then
-        bit="0 B"
-    elif [ "$1" -lt 100 ]; then
-        bit="$(echo "scale=0;$1*8" | bc -l ) B"
-    elif [ "$1" -lt 100000 ]; then
-        bit="$(echo "scale=0;$1*8/1000" | bc -l ) K"
-    else
-        bit="$(echo "scale=1;$1*8/1000000" | bc -l ) M"
-    fi
+# print_bit() {
+#     if [ "$1" -eq 0 ] || [ "$1" -lt 10 ]; then
+#         bit="0 B"
+#     elif [ "$1" -lt 100 ]; then
+#         bit="$(echo "scale=0;$1*8" | bc -l ) B"
+#     elif [ "$1" -lt 100000 ]; then
+#         bit="$(echo "scale=0;$1*8/1000" | bc -l ) K"
+#     else
+#         bit="$(echo "scale=1;$1*8/1000000" | bc -l ) M"
+#     fi
 
-    echo "$bit"
-}
+#     echo "$bit"
+# }
 
-INTERVAL=1
+INTERVAL=0.5
 INTERFACES=`ip link | grep 'state UP' | cut -d ':' -f 2 | cut -c 2-`
 
 declare -A bytes
@@ -39,13 +39,12 @@ done
 while true; do
     down=0
     up=0
-
     for interface in $INTERFACES; do
         bytes[now_rx_$interface]="$(cat /sys/class/net/"$interface"/statistics/rx_bytes)"
         bytes[now_tx_$interface]="$(cat /sys/class/net/"$interface"/statistics/tx_bytes)"
 
-        bytes_down=$((((${bytes[now_rx_$interface]} - ${bytes[past_rx_$interface]})) / INTERVAL))
-        bytes_up=$((((${bytes[now_tx_$interface]} - ${bytes[past_tx_$interface]})) / INTERVAL))
+        bytes_down=$(echo "scale=0;(${bytes[now_rx_$interface]}-${bytes[past_rx_$interface]})/$INTERVAL" | bc)
+        bytes_up=$(echo "scale=0;(${bytes[now_tx_$interface]}-${bytes[past_tx_$interface]})/$INTERVAL" | bc)
 
         down=$(((( "$down" + "$bytes_down" ))))
         up=$(((( "$up" + "$bytes_up" ))))
@@ -54,7 +53,7 @@ while true; do
         bytes[past_tx_$interface]=${bytes[now_tx_$interface]}
     done
 
-    echo " $(print_bytes $down)"
+    printf " %7s    %7s\n" "$(print_bytes $up)" "$(print_bytes $down)"
     # echo "Download: $(print_bit $down) / Upload: $(print_bit $up)"
 
     sleep $INTERVAL
