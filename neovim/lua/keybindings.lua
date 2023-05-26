f2a0f95dc368a79775dbaa11aea7d690929b04eb
CrackTC <chenrke369@gmail.com>
2023-05-27 02:53:08 +0800

    nvim: migrate keybindings to lua

--- neovim/lua/keybindings.lua
+++ neovim/lua/keybindings.lua
@@ -1,107 +1,131 @@
-vim.cmd([[
-" ===
-" === Basic Mappings
-" ===
-let mapleader=" "
-noremap ; :
+-- [[key bindings]] --
 
-nnoremap S :w<CR>
-nnoremap Q :q<CR>
+vim.g.mapleader = ' '
 
-nnoremap Y y$
+local map = vim.keymap.set
 
-nnoremap < <<
-nnoremap > >>
+local function noremap(mode, lhs, rhs, opts)
+    opts = opts or {}
+    opts.noremap = true
+    map(mode, lhs, rhs, opts)
+end
 
-noremap N $
-noremap P 0
-noremap H 5h
-noremap J 5j
-noremap K 5k
-noremap L 5l
+local function nnoremap(lhs, rhs, opts)
+    noremap('n', lhs, rhs, opts)
+end
 
-nnoremap <A-n> 5<C-e>
-nnoremap <A-p> 5<C-y>
+local function vnoremap(lhs, rhs, opts)
+    noremap('v', lhs, rhs, opts)
+end
 
-" ===
-" === Searching
-" ===
-nnoremap - N
-nnoremap = n
-noremap <LEADER><CR> :nohlsearch<CR>
+local function cnoremap(lhs, rhs, opts)
+    noremap('c', lhs, rhs, opts)
+end
 
-" ===
-" === Window Management
-" ===
-noremap s <nop>
+local function tnoremap(lhs, rhs, opts)
+    noremap('t', lhs, rhs, opts)
+end
 
-nnoremap sl :set splitright<CR>:vsplit<CR>
-nnoremap sh :set nosplitright<CR>:vsplit<CR>:set splitright<CR>
-nnoremap sj :set splitbelow<CR>:split<CR>
-nnoremap sk :set nosplitbelow<CR>:split<CR>:set splitbelow<CR>
+local function nmap(lhs, rhs, opts)
+    map('n', lhs, rhs, { remap = true })
+end
 
-nnoremap gj <C-w>j
-nnoremap gk <C-w>k
-nnoremap gh <C-w>h
-nnoremap gl <C-w>l
 
-noremap <UP> :resize +5<CR>
-noremap <DOWN> :resize -5<CR>
-noremap <LEFT> :vertical resize-5<CR>
-noremap <RIGHT> :vertical resize+5<CR>
+-- [basic mappings] --
 
-" ===
-" === Tab Management
-" ===
-nnoremap tj :-tabe<CR>
-nnoremap tk :tabe<CR>
+nnoremap(';', ':')
+vnoremap(';', ':')
 
-nnoremap th :-tabnext<CR>
-nnoremap tl :+tabnext<CR>
+nnoremap('Y', 'y$')
+nmap('<leader>rr', 'r$')
 
-nnoremap tmh :-tabmove<CR>
-nnoremap tml :+tabmove<CR>
+nnoremap('S', ':w<CR>')
+nnoremap('Q', ':q<CR>')
 
-nnoremap <LEADER>1 1gt
-nnoremap <LEADER>2 2gt
-nnoremap <LEADER>3 3gt
-nnoremap <LEADER>4 4gt
-nnoremap <LEADER>5 5gt
-nnoremap <LEADER>6 6gt
-nnoremap <LEADER>7 7gt
-nnoremap <LEADER>8 8gt
-nnoremap <LEADER>9 9gt
+nnoremap('<', '<<')
+nnoremap('>', '>>')
 
-" ===
-" === Terminal Behavior
-" ===
-autocmd TermOpen term://* startinsert
-tnoremap <C-q> <C-\><C-n>
-tnoremap <C-o> <C-\><C-n><C-o>
-tnoremap <C-h> <C-\><C-n><C-w>h
-tnoremap <C-j> <C-\><C-n><C-w>j
-tnoremap <C-k> <C-\><C-n><C-w>k
-tnoremap <C-l> <C-\><C-n><C-w>l
+nnoremap('N', '$')
+nnoremap('P', '0')
 
-" Open Simple Terminal(St) With The Cwd
-nnoremap <LEADER>st :tabe<CR>:-tabmove<CR>:term sh -c 'st'<CR><C-\><C-n>:q<CR>
+nnoremap('H', '5h')
+nnoremap('J', '5j')
+nnoremap('K', '5k')
+nnoremap('L', '5l')
 
-" Open A New Terminal Window
-nnoremap <LEADER>ty :set splitbelow<CR>:split<CR>:resize +10<CR>:term<CR>
+vnoremap('H', '5h')
+vnoremap('J', '5j')
+vnoremap('K', '5k')
+vnoremap('L', '5l')
 
-" ===
-" === Insert Mode Cursor Movement
-" ===
-" Move To The End Of A Line
-inoremap <C-a> <ESC>A
+nnoremap('<A-n>', '5<C-e>')
+nnoremap('<A-p>', '5<C-y>')
 
-" ===
-" === Command Mode Cursor Movement
-" ===
-cnoremap <C-a> <HOME>
-cnoremap <C-e> <END>
-cnoremap <C-p> <LEFT>
-cnoremap <C-n> <RIGHT>
+-- [searching] --
 
-nmap <LEADER>rr r$
-]])
+nnoremap('-', 'N')
+nnoremap('=', 'n')
+nnoremap('<leader><CR>', ':nohlsearch<CR>', { silent = true })
+
+-- [window management] --
+
+nnoremap('s', '<nop>')
+nnoremap('sl', ':set splitright<CR>:vsplit<CR>')
+nnoremap('sh', ':set nosplitright<CR>:vsplit<CR>:set splitright<CR>')
+nnoremap('sj', ':set splitbelow<CR>:split<CR>')
+nnoremap('sk', ':set nosplitbelow<CR>:split<CR>:set splitbelow<CR>')
+
+nnoremap('gj', '<C-w>j')
+nnoremap('gk', '<C-w>k')
+nnoremap('gh', '<C-w>h')
+nnoremap('gl', '<C-w>l')
+
+nnoremap('<UP>', ':resize +5<CR>')
+nnoremap('<DOWN>', ':resize -5<CR>')
+nnoremap('<LEFT>', ':vertical resize-5<CR>')
+nnoremap('<RIGHT>', ':vertical resize+5<CR>')
+
+-- [tab management] --
+nnoremap('tj', ':-tabe<CR>')
+nnoremap('tk', ':tabe<CR>')
+
+nnoremap('th', ':-tabnext<CR>')
+nnoremap('tl', ':+tabnext<CR>')
+
+nnoremap('tmh', ':-tabmove<CR>')
+nnoremap('tml', ':+tabmove<CR>')
+
+nnoremap('<leader>1', '1gt')
+nnoremap('<leader>2', '2gt')
+nnoremap('<leader>3', '3gt')
+nnoremap('<leader>4', '4gt')
+nnoremap('<leader>5', '5gt')
+nnoremap('<leader>6', '6gt')
+nnoremap('<leader>7', '7gt')
+nnoremap('<leader>8', '8gt')
+nnoremap('<leader>9', '9gt')
+
+-- [terminal behavior] --
+vim.api.nvim_create_autocmd('TermOpen', {
+    pattern = 'term://*',
+    command = 'startinsert'
+})
+
+tnoremap('<C-q>', [[<C-\><C-n>]])
+tnoremap('<C-o>', [[<C-\><C-n><C-o>]])
+tnoremap('<C-h>', [[<C-\><C-n><C-w>h]])
+tnoremap('<C-j>', [[<C-\><C-n><C-w>j]])
+tnoremap('<C-k>', [[<C-\><C-n><C-w>k]])
+tnoremap('<C-l>', [[<C-\><C-n><C-w>l]])
+
+-- open kitty in cwd
+nnoremap('<leader>tt', function()
+    os.execute('kitty&')
+end)
+
+-- [command mode cursor movement] --
+cnoremap('<C-a>', '<Home>')
+cnoremap('<C-e>', '<End>')
+cnoremap('<C-p>', '<Left>')
+cnoremap('<C-n>', '<Right>')
+
