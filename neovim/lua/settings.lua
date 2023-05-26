e2d83b3b2e9037a361859e7911dfbdf20456a3c9
CrackTC <chenrke369@gmail.com>
2023-05-27 01:48:52 +0800

    nvim: migrating settings to lua

--- neovim/lua/settings.lua
+++ neovim/lua/settings.lua
@@ -1,56 +1,71 @@
-vim.cmd([[
-" ===
-" === System
-" ===
-let t_ut=''
-set clipboard+=unnamedplus
-set mouse=
-set fileencodings=ucs-bom,utf-8,default,gbk,latin1
+-- [[main settings for neovim]] --
 
-" ===
-" === Editor Behavior
-" ===
-set number
-set relativenumber
-set cursorline
-set hlsearch
-set ruler
-set expandtab
-set tabstop=4
-set shiftwidth=4
-set autoindent
-set list
-set listchars=tab:\|\ ,trail:▫
-set scrolloff=4
-set nowrap
-" set linebreak " Reserve Whole Word While Wrapping
-set timeoutlen=500
-set textwidth=0
-set indentexpr=""
-set foldmethod=indent
-set foldlevel=99
-set foldenable
-set noshowmode
-set hidden
-set showcmd
-set wildmenu
-set ignorecase
-set smartcase
-set shortmess+=c
-set inccommand=split " Quick Preview While Replacing Words
-set completeopt=longest,noinsert,menuone,noselect,preview " Build-in Completion Config
-set lazyredraw
-silent !mkdir -p ~/.config/nvim/tmp/backup
-set backupdir=~/.config/nvim/tmp/backup,.
-set directory=~/.config/nvim/tmp/backup,.
-silent !mkdir -p ~/.config/nvim/tmp/undo
-if has('persistent_undo')
-	set undofile
-	set undodir=~/.config/nvim/tmp/undo,.
-endif
-set updatetime=90
-set virtualedit=block
+vim.o.mouse = ""                                                -- disable mouse
+vim.o.clipboard = vim.o.clipboard .. "unnamedplus"              -- use system clipboard
+vim.o.fileencodings = "ucs-bom,utf-8,default,gbk,latin1"
 
-autocmd BufReadPost * if line("'\"") > 1 && line("'\"") <= line("$") | exe "normal! g'\"" | endif
-exec "nohlsearch"
-]])
+-- [editor behavior] --
+
+vim.o.number = true                                             -- show line numbers
+vim.o.relativenumber = true                                     -- show relative line numbers
+vim.o.cursorline = true                                         -- highlight current line
+vim.o.hlsearch = true                                           -- highlight search results
+vim.o.ruler = true                                              -- show cursor position
+vim.o.expandtab = true                                          -- use spaces instead of tabs
+vim.o.tabstop = 4                                               -- tab size, number of spaces
+vim.o.shiftwidth = 4                                            -- indent size, number of spaces, used for >>, <<, ==, etc.
+vim.o.autoindent = true                                         -- copy indent from current line when starting a new line
+vim.o.list = true                                               -- show whitespace characters
+vim.o.listchars = [[tab:󰌒 ,trail:·]]                            -- whitespace characters
+vim.o.scrolloff = 4                                             -- minimum number of lines to keep above and below cursor
+vim.o.wrap = false                                              -- don't wrap lines
+vim.o.linebreak = false                                         -- wrap long lines at characters in 'breakat'
+vim.o.timeoutlen = 500                                          -- time to wait for a mapped sequence to complete (in milliseconds)
+vim.o.textwidth = 0                                             -- don't wrap lines
+
+vim.o.foldenable = true                                         -- enable folding
+vim.o.foldmethod = "indent"                                     -- fold based on indent
+vim.o.foldlevel = 99                                            -- open all folds by default
+
+vim.o.showmode = false                                          -- don't show mode
+vim.o.hidden = true                                             -- allow switching between buffers without saving
+vim.o.showcmd = true                                            -- show command in bottom bar
+vim.o.wildmenu = true                                           -- show completion menu
+vim.o.ignorecase = true                                         -- ignore case when searching
+vim.o.smartcase = true                                          -- ignore case when searching lowercase, but not when searching uppercase
+vim.o.shortmess = vim.o.shortmess .. "c"                        -- don't show completion messages
+vim.o.inccommand = "split"                                      -- quick preview while replacing words
+vim.o.completeopt = "longest,noinsert,menuone,noselect,preview" -- build-in completion config
+vim.o.lazyredraw = true                                         -- don't redraw while executing macros
+vim.o.virtualedit = "block"                                     -- allow cursor to move anywhere in visual block mode
+
+-- [backup] --
+
+if not vim.fn.isdirectory(vim.fn.expand("~/.config/nvim/tmp/backup")) then
+    vim.fn.mkdir(vim.fn.expand("~/.config/nvim/tmp/backup"), "p")
+end
+
+if not vim.fn.isdirectory(vim.fn.expand("~/.config/nvim/tmp/undo")) then
+    vim.fn.mkdir(vim.fn.expand("~/.config/nvim/tmp/undo"), "p")
+end
+
+vim.o.backupdir = "~/.config/nvim/tmp/backup,."
+vim.o.directory = "~/.config/nvim/tmp/backup,."
+
+if vim.fn.has("persistent_undo") then
+    vim.o.undofile = true
+    vim.o.undodir = "~/.config/nvim/tmp/undo"
+end
+
+vim.o.updatetime = 90                                           -- time to wait for a mapped sequence to complete (in milliseconds)
+
+-- [recover cursor position] --
+
+vim.api.nvim_create_autocmd("BufReadPost", {
+    pattern = "*",
+    callback = function()
+        if vim.fn.line("'\"") > 1 and vim.fn.line("'\"") <= vim.fn.line("$") then
+            vim.cmd([[normal! g`"]])
+        end
+    end
+})
