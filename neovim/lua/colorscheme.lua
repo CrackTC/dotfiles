79a803e67268a5e23a2de5dcd46d99ad2098327b
CrackTC <chenrke369@gmail.com>
2023-05-28 13:44:25 +0800

    nvim: colorscheme: migrate to lua

--- neovim/lua/colorscheme.lua
+++ neovim/lua/colorscheme.lua
@@ -1,54 +1,51 @@
+-- [[color scheme]] --
+
+vim.opt.termguicolors = true
+vim.opt.background = "dark"
+
+-- [nord] --
+vim.g.nord_bold = true
+vim.g.nord_italic = true
+vim.g.nord_italic_comments = true
+vim.g.nord_underline = true
+vim.g.nord_cursor_line_number_background = true
+vim.g.nord_uniform_status_lines = true
+vim.g.nord_bold_vertical_split_line = true
+vim.g.nord_uniform_diff_background = true
+
+vim.cmd.colorscheme("nord")
+
+vim.cmd([[hi! Normal          ctermbg = NONE guibg = #2E3440]])
+vim.cmd([[hi! SignColumn      ctermbg = NONE guibg = NONE   ]])
+vim.cmd([[hi! VertSplit       ctermbg = NONE guibg = NONE   ]])
+vim.cmd([[hi! NonText         ctermfg = gray guifg = #434c5e]])
+vim.cmd([[hi! CocMarkdownLink ctermfg = 12   guifg = #8fbcbb]])
+vim.cmd([[hi! CocInlayHint    ctermfg = 12   guifg = #616e88]])
+
+vim.g.coc_status_error_sign = "󰈸 "
+vim.g.coc_status_warning_sign = " "
+vim.g.lightline = {
+    colorscheme        = "nord",
+    component          = { lineinfo = " %3l:%-2c", },
+    separator          = { left = "", right = "" },
+    subseparator       = { left = "", right = "" },
+    component_function = {
+        readonly   = "LightlineReadonly",
+        fugitive   = "LightlineFugitive",
+        cocstatus  = "LightlineCoc",
+        filetype   = "MyFiletype",
+        fileformat = "MyFileformat",
+        git        = "GitStatus",
+    },
+    active             = {
+        left = { { "filename" }, { "git", "cocstatus", "readonly", "fugitive", "modified" } },
+    },
+}
+
+vim.g.lightline.tabline_separator = vim.g.lightline.separator
+vim.g.lightline.tabline_subseparator = vim.g.lightline.subseparator
+
 vim.cmd([[
-set termguicolors
-
-set background=dark
-
-" nord
-let g:nord_bold                          = 1
-let g:nord_italic                        = 1
-let g:nord_italic_comments               = 1
-let g:nord_underline                     = 1
-let g:nord_cursor_line_number_background = 1
-let g:nord_uniform_status_lines          = 1
-let g:nord_bold_vertical_split_line      = 1
-let g:nord_uniform_diff_background       = 1
-
-colorscheme nord
-
-hi! Normal          ctermbg = NONE   guibg = #2E3440
-hi! SignColumn      ctermbg = NONE   guibg = NONE
-hi! VertSplit       ctermbg = NONE   guibg = NONE
-hi! NonText         ctermfg = gray   guifg = #434c5e
-hi! CocMarkdownLink ctermfg = 12     guifg = #8fbcbb
-hi! CocInlayHint    ctermfg = 12     guifg = #616e88
-
-let g:coc_status_error_sign   = '󰈸 '
-let g:coc_status_warning_sign = ' '
-
-let g:lightline = {
-			\ 'colorscheme' : "nord",
-			\ 'component': {
-			\   'lineinfo': ' %3l:%-2c'
-			\ },
-			\ 'separator': { 'left': '', 'right': '' },
-			\ 'subseparator': { 'left': '', 'right': '' },
-			\ 'component_function': {
-			\   'readonly'   : 'LightlineReadonly',
-			\   'fugitive'   : 'LightlineFugitive',
-			\   'cocstatus'  : 'LightlineCoc',
-			\   'filetype'   : 'MyFiletype',
-			\   'fileformat' : 'MyFileformat',
-			\   'git'        : 'GitStatus',
-			\ },
-			\ 'active'       : {
-			\ 'left': [ [ 'filename' ],
-			\             [ 'git', 'cocstatus', 'readonly', 'fugitive', 'modified' ] ]
-			\ }
-			\ }
-
-let g:lightline.tabline_separator = g:lightline.separator
-let g:lightline.tabline_subseparator = g:lightline.subseparator
-
 function! LightlineCoc()
   if empty(get(g:, 'coc_status', '')) && empty(get(b:, 'coc_diagnostic_info', {}))
     return ''
@@ -68,8 +65,6 @@ function! LightlineFugitive()
     return ''
 endfunction
 
-autocmd User CocStatusChange,CocDiagnosticChange call lightline#update()
-
 function! MyFiletype()
   return winwidth(0) > 70 ? (strlen(&filetype) ? &filetype . ' ' . WebDevIconsGetFileTypeSymbol() : 'no ft') : ''
 endfunction
@@ -83,3 +78,8 @@ function! GitStatus()
   return printf('+%d ~%d -%d', a, m, r)
 endfunction
 ]])
+
+vim.api.nvim_create_autocmd("User", {
+    pattern = {"CocStatusChange", "CocDiagnosticChange"},
+    command = "call lightline#update()"
+})
